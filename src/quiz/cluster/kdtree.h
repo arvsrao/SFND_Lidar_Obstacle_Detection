4163f4261d2c6808e4a0de4dc93c69ec65e35a67
Arvind Rao <arvsrao@gmail.com>
2021-03-29 10:22:58 +0200

    begin kd tree search function

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -36,17 +36,71 @@ struct KdTree
 		delete root;
 	}
 
+	/** helper function inserts node into tree */
+	void insertNode(Node** parent, Node* newNode, uint depth) {
+
+        if (*parent == NULL) {
+            *parent = newNode;
+            return;
+        }
+
+        // based on depth compare x or y coordinate
+        uint idx = depth % 2;
+
+        // left - right based on y-value
+        if (newNode->point.at(idx) >= (*parent)->point.at(idx)) {
+            insertNode(&((*parent)->right), newNode, depth + 1);
+        }
+        else {
+            insertNode(&((*parent)->left), newNode, depth + 1);
+        }
+	}
+
+	/**
+	 * Insert a new point into the tree
+	 *
+	 * @param point
+	 * @param id
+	 */
 	void insert(std::vector<float> point, int id)
 	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// create a new node
+		Node* newNode = new Node(point, id);
 
+		// place the newly created node correctly with in the root
+		insertNode(&root, newNode, 0);
 	}
+
+    /** helper function searches tree for points in the neighborhood of a target point. */
+    void searchTree(Node* parent, std::vector<float> target, float distanceTol, uint depth) {
+
+        if (parent == NULL) { 
+            return;
+        }
+
+        // test if parent is in box and if yes test if it is in the neighborhood of
+        // of the target point. Otherwise, continue traversing the tree
+
+        // based on depth compare x or y coordinate
+        uint idx = depth % 2;
+
+        // left - right based on y-value
+        if (target.at(idx) >= parent->point.at(idx)) {
+            searchTree(parent->right, target, distanceTol, depth + 1);
+        }
+        else {
+            searchTree(parent->left, target, distanceTol, depth + 1);
+        }
+    }
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+
+		// try to
+
+
 		return ids;
 	}
 	
